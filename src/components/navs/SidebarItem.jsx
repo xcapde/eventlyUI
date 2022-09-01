b034e6d9476a6d4839a973f91e61e5978d4a3fd9
Agnès Font Torres <afonttorres@gmail.com>
2022-09-01 21:30:38 +0200

    Link added

--- src/components/navs/SidebarItem.jsx
+++ src/components/navs/SidebarItem.jsx
@@ -1,17 +1,21 @@
+import { Link } from "react-router-dom"
 import { SideBarIcon, SideBarItemIconCol, SideBarItemContent, SideBarItemRow } from "./sidebar.styled"
 
 export const SidebarItem = ({ sub, content, callback }) => {
 
     return (
         <SideBarItemRow onMouseEnter={callback ? (e) => callback(e) : null}>
+            <Link to={content.link}>
+                <SideBarItemIconCol >
+                    <SideBarIcon>{content.icon}</SideBarIcon>
+                </SideBarItemIconCol>
 
-            <SideBarItemIconCol >
-                <SideBarIcon>{content.icon}</SideBarIcon>
-            </SideBarItemIconCol>
+                <SideBarItemContent id="sidebar-content" sub={sub}>
+                    {content.content}
+                </SideBarItemContent>
+            </Link>
 
-            <SideBarItemContent id="sidebar-content" sub={sub}>
-                {content.content}
-            </SideBarItemContent>
+
 
         </SideBarItemRow >
     )
