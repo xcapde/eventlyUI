2b6fe9c95c272d740350e7a084b0b42d2ec528d1
xcapde <xcapde@hotmail.com>
2022-09-02 08:39:55 +0200

    MainCard fixed

--- src/components/feeds/EventFeed.jsx
+++ src/components/feeds/EventFeed.jsx
@@ -1,12 +1,30 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import { Col } from "../../styles/styles.styled";
 import { MainCard } from "../cards/MainCard";
 
 export const EventFeed = () => {
+    // const [events, setEvents] = useState([]);
+
+    // useEffect(()=>{
+
+    // },[])
+
+    // const getAllEvents=()=>{
+
+    // }
 
     return (
         <Col style={{width: '100%', height: '100%'}}>
+            
+            {/* <React.Fragment>{events.map((event,key) =>
+                <MainCard key={key} event={event}/>
+                ).reverse()}
+            </React.Fragment> */}
             <MainCard />
+
+            
         </Col>
     )
 }
