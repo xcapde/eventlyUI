b88e62db35cba66a7ae6aff4dd514a8d16f354fa
xcapde <xcapde@hotmail.com>
2022-09-12 10:09:28 +0200

    SmallCard ModuleJoin size refactor

--- src/components/information/ModuleJoin.jsx
+++ src/components/information/ModuleJoin.jsx
@@ -5,7 +5,7 @@ import { Participation } from "../subcomponents/Participation"
 export const ModuleJoin = ({event, height}) => {
     
     return (
-        <Row height={height} justifyContent={'space-between'}>
+        <Row height={height} style={{justifyContent:'space-between', gap:'0.5rem', width:'95%', margin:' 2%'}}>
             <Participation event={event}/>
             <PrimaryButton content={'JOIN'} callback={()=>console.error('not implemented yet')}/>
         </Row>
