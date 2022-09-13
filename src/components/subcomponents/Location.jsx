7c5481b6a594d8a9650d9c4814137e7f2f4146b4
Agnès Font Torres <afonttorres@gmail.com>
2022-09-13 15:20:48 +0200

    features/Upload

--- src/components/subcomponents/Location.jsx
+++ src/components/subcomponents/Location.jsx
@@ -2,7 +2,7 @@ import { DetailText } from "../../styles/styles.styled"
 
 export const Location = ({ event }) => {
     return (
-        <DetailText style={{textTransform:'capitalize'}}>
+        <DetailText style={{ textTransform: 'capitalize', lineHeight: '20px' }}>
             <i className="fa-solid fa-location-dot"></i>
             {event.location ? `${event.location.substring(0, (event.location.lastIndexOf(',') - 3))}` : 'location'}
         </DetailText>
