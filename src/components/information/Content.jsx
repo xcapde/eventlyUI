95b8b33491b5b1a0bf826aec64190db6c5f18707
xcapde <xcapde@hotmail.com>
2022-09-14 02:57:10 +0200

    key warning in details solved

--- src/components/information/Content.jsx
+++ src/components/information/Content.jsx
@@ -10,7 +10,7 @@ export const Content = ({ field, event }) => {
     return (
         <Col>
             {field && (field !== "description" && field !== "map") ?
-                event[field].map(item => <Badge content={item} field={field} />)
+                event[field].map((item, key) => <Badge key={key} content={item} field={field} />)
                 : field === "description" ? <h1>{event[field]}</h1> : 'NO FIELD IN EVENT (MAP)'
             }
         </Col>
