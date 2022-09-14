adf3eda277b943fbaf413d3c322713a6372112f4
Agnès Font Torres <afonttorres@gmail.com>
2022-09-14 22:04:07 +0200

    provisional flex model in Profile

--- src/components/profile/Porfile.jsx
+++ src/components/profile/Porfile.jsx
@@ -1,18 +1,27 @@
 import { useEffect, useState } from "react"
 import { eventService } from "../../services/API/eventService"
-import { Col, Title, View } from "../../styles/styles.styled"
+import { AuthService } from "../../services/AuthService"
+import { Col, DetailText, Title, View } from "../../styles/styles.styled"
 import { Footer } from "../footer/Footer"
 import { ProfileGrid } from "./profile.styled"
 
 export const Profile = () => {
 
     useEffect(() => {
+        getAuth();
         getJoined();
         getPublished();
     }, [])
 
+    const [username, setUsername] = useState();
     const [joined, setJoined] = useState();
     const [published, setPublished] = useState();
+
+
+    const getAuth = () => {
+        if (!AuthService.getAuthUser().username) return;
+        setUsername(AuthService.getAuthUser().username);
+    }
 
 
     const getJoined = () => {
@@ -31,24 +40,30 @@ export const Profile = () => {
         })
     }
 
-    console.log(published);
-    return (
-        // <View style={{height:'90vh', top:0}}>       
+    console.log(username)
 
-        <ProfileGrid>
-            <Col>
-                <Title>Joined Events</Title>
-                {joined && joined.length > 0 &&
-                    joined.map((event, key) => (<span key={key}>{event.title}</span>))}
+    return (
+        <View style={{ height: '90vh', top: 0, width: '85%', left: 'calc((100% - 85%) / 2)' }}>
+            {/* <ProfileGrid> */}
+            <Col style={{ height: '20vh', gap: '5%' }}>
+                <Title>Hi {username}!</Title>
             </Col>
-            <Col>
+            <Col style={{ height: '40vh', gap: '5%'  }}>
+                <Title>Joined Events</Title>
+                {joined && joined.length > 0 ?
+                    joined.map((event, key) => (<span key={key}>{event.title}</span>)) :
+                    <DetailText>You did not join any event yet!</DetailText>}
+            </Col>
+            <Col style={{ height: '40vh' , gap: '5%' }}>
                 <Title>Published Events</Title>
-                {published && published.length > 0 &&
-                    published.map((event, key) => (<span key={key}>{event.title}</span>))}
+                {published && published.length > 0 ?
+                    published.map((event, key) => (<span key={key}>{event.title}</span>)) :
+                    <DetailText>What are you waiting to publish your first event?!</DetailText>
+                }
             </Col>
             <Footer />
-        </ProfileGrid>
+            {/* </ProfileGrid> */}
 
-        // </View>
+        </View>
     )
 }
