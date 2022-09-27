88d4360616be2ef0e8b2fe1a4b5ed214155ebf64
Agnès Font Torres <afonttorres@gmail.com>
2022-09-27 17:36:55 +0200

    base url -> /api/

--- src/services/API/authService.js
+++ src/services/API/authService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import validation from "../../utils/validation";
 
-const baseURL = "http://localhost:8080";
+const baseURL = "http://localhost:8080/api";
 export const authService = {
 
     signup(data) {
--- src/services/configuration/axios-config.js
+++ src/services/configuration/axios-config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { AuthService } from "../AuthService";
 
-axios.defaults.baseURL = `http://localhost:8080`;
+axios.defaults.baseURL = `http://localhost:8080/api`;
 axios.defaults.headers.post["Content-Type"] = "application/json";
 axios.defaults.headers.post["Accept"] = "application/json";
 axios.defaults.withCredentials = false;
